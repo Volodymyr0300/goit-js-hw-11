78cb35937cc4ab91d72dbeddfb08fd52f5a15423
Volodymyr Poliakov <vladimir03002@gmail.com>
2023-05-22 14:10:09 -0500

    add another one library

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 const axios = require('axios/dist/browser/axios.cjs'); // browser
 const axios = require('axios/dist/node/axios.cjs'); // node
-// all modules
-import Notiflix from 'notiflix';
-// one by one
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import { Report } from 'notiflix/build/notiflix-report-aio';
-import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
-import { Loading } from 'notiflix/build/notiflix-loading-aio';
-import { Block } from 'notiflix/build/notiflix-block-aio';
