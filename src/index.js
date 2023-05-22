3acf53a6c9bfa0db5aa2bdcdf71050d8cc5a6882
Volodymyr Poliakov <vladimir03002@gmail.com>
2023-05-22 14:02:45 -0500

    add some libraries

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const axios = require('axios/dist/browser/axios.cjs'); // browser
+const axios = require('axios/dist/node/axios.cjs'); // node
+// all modules
+import Notiflix from 'notiflix';
+// one by one
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Report } from 'notiflix/build/notiflix-report-aio';
+import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
+import { Loading } from 'notiflix/build/notiflix-loading-aio';
+import { Block } from 'notiflix/build/notiflix-block-aio';
